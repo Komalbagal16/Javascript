41915e191acfedb84d2a49b054a148883aa459d1
Komal Bagal <bagalkomal75@gmail.com>
2024-09-11 19:24:22 +0530

    Add files via upload

---
+++ 9 Aug ifElse.js
@@ -0,0 +1,61 @@
+//ifelse in javascript
+
+//syntax:
+//  if(condition)//Expresstion{
+//    //code to be executrd if condition is true
+//} else{
+//   //code be executed if condition is true
+//}
+
+// let age = 1;
+// if(age >= 18){
+//     console.log("you can vote")
+// } else{
+//     console.log("you can't vote")
+// }
+
+// //n= ? (20-------50)
+// let num=23
+// if(num>=20 && num<=50){
+//     console.log("number is between 20 and 50");
+// }else{
+//     console.log("number is not between 20and 50")
+// }
+
+// let y=2020;
+// if(y % 4==0)
+// {
+//     console.log("is a leap year")
+// }else{
+//     console.log("is not aleap year")
+// }
+
+// you can't vote
+// ifElse.js:21 number is between 20 and 50
+// // ifElse.js:29 is a leap year
+
+// let a= 5;
+// let b= 5;
+// let c= 5;
+// if(a==b && b==c) {
+//     console.log("Equilateral triangle");
+// }
+// else if(a*a==b*b+c*c || b*b==a*a+c*c || c*c==a*a+b*b){
+//     console.log("Right angle triangle");
+// }
+// else if(a==b || a==c || b==c){
+//     console.log("Isosceles triangle");
+// }
+// else if (a!=b && b!=c && a!=c){
+//     console.log("Scalene triangle");
+// }
+// else {
+//     console.log("Not a triangle");
+// }
+
+//find the type of  tringle
+
+
+
+
+//
