b9e174e7bb5b7353670db909e2013be2986cdf61
Komal Bagal <bagalkomal75@gmail.com>
2024-09-11 19:22:11 +0530

    Add files via upload

---
+++ 8 Aug script1.js
@@ -0,0 +1,64 @@
+var a = 8;
+console.log(a);
+console.log(typeof a);
+
+var b ='hello'
+console.log(b)
+console.log(typeof b);
+
+var a ='bye'
+console.log(a)
+console.log(typeof a);
+
+let x= 55;
+console.log(x);
+console.log(typeof x);
+
+// let x;
+// SyntaxError: Identifier 'x' has already been 
+// declared (at script1.js:17:5)
+
+x=55;
+console.log(x)
+console.log(typeof x);
+
+const n= 2.17
+console.log(n)
+console.log(typeof n);
+
+// n=5678;
+// TypeError: Assignment to constant variable.
+
+var y = 10;
+console.log(y);
+console.log(typeof y)
+{
+    y=23
+    console.log(y);
+}
+console.log(y);
+
+const g='vaishnavi'
+console.log(g);
+console.log(typeof g);
+
+let k=12;
+console.log(k);
+console.log(typeof k);
+{
+    let k='komal'
+    console.log(k);
+    console.log(typeof k);
+}
+console.log(y);
+
+const p=1;
+console.log(p);
+{
+    f='ram';
+    console.log(f);
+    var l='seeta'
+    console.log(l);
+}
+console.log(x);
+
