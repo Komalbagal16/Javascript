cd0e369f249b8c078bacf3049042b6cc0ba026ca
Komal Bagal <bagalkomal75@gmail.com>
2024-09-11 19:28:52 +0530

    Add files via upload

---
+++ 14 Aug index.js
@@ -0,0 +1,70 @@
+// While loop in javascript//
+
+// let i= 0;
+// while (i <= 5){
+//  console.log(2**i);
+// i++;
+// }
+
+// o/p:
+// 1
+// 2
+// 4
+// 8
+// 16
+// 32
+
+//i/p : 234
+//o/p : 432
+
+// let n = 234;
+// let rev = 0;
+// while(n!= 0){
+//     let rem = n % 10;
+//     rev = rev * 10 + rem;
+//     n = Math.floor(n / 10);
+// }
+// console.log("rev :", rev);
+
+// o/p rev : 432
+
+// i/p: 1101
+// o/p: 13
+
+// let n = 1101;
+// let sum = 0;
+// let power = 0;
+// while(n > 0){
+//     let rem = n % 10;
+//     sum += rem* Math.pow(2,power);
+//     n = Math.floor(n/10);
+//     power++;
+// }
+// console.log("decimal :",sum);
+
+// o/p: decimal : 13
+
+//i/p: 15
+//o/p: 1111
+
+// let n = 15;
+// let binary = "";
+// while(n > 0){
+//     let r= n % 2;
+//     binary = r + binary;
+//     n = Math.floor(n/2);
+// }
+// console.log("binary :", Number(binary));
+
+//o/p: binary : 1111
+
+// Do-While= Exist Control loop / Exit Cheaking loop.
+// Password Verification:
+
+// let password;
+// do {
+//   password = prompt("Enter your password");
+// } while(password != "1234");
+// console.log("Your Device is Unlocked💕");
+
+//O/P: Your Device is Unlocked💕
