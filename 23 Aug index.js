747d4e1da249f3e2dff1b4c79176c4b76ede9a38
Komal Bagal <bagalkomal75@gmail.com>
2024-09-11 19:43:42 +0530

    Add files via upload

---
+++ 23 Aug index.js
@@ -0,0 +1,59 @@
+// // sum =  undefined
+
+// function fibo(n) {
+//   if (n == 0) {
+//     return;
+//   }
+//   if (n == 1) {
+//     return 0;
+//   }
+//   if (n == 2) {
+//     return 1;
+//   }
+//   return fibo(n - 1) + fibo(n - 2);
+// }
+// for (let i = 0; i <= 10; i++) {
+//   console.log(fibo(i));
+// }
+
+//undefined
+// index.js:34 0
+// index.js:34 1
+// index.js:34 2
+// index.js:34 3
+// index.js:34 5
+// index.js:34 8
+// index.js:34 13
+// index.js:34 21
+// index.js:34 34
+
+//Anonymous function/
+//Anonymous function are functions that are
+
+// function sum(a, b) {
+//   return a + b;
+// }
+// let ans = sum(3, 5);
+// console.log(ans);
+
+// // 8
+
+// //or
+// let newSum = sum;
+// // console.log(sum(30,20))
+// console.log(newSum(30,20))
+
+// let newSum = function (a, b){
+//     return a+ b;
+// }
+// console.log(newSum(34,78));
+
+// 112
+
+// setTimeout(function () {
+//   console.log("hello");
+// }, 3000);
+
+///hello
+
+//iife
