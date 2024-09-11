e362570d8fc38734d0b3d30c81361066242446c2
Komal Bagal <bagalkomal75@gmail.com>
2024-09-11 19:25:08 +0530

    Add files via upload

---
+++ 9 Aug script.js
@@ -0,0 +1,133 @@
+// let a=15;
+// let b=5;
+// // operations in javascript
+// Arithemetic operations: +,-,*,/,%,** 
+
+// console.log("a + b =",a + b);
+// console.log("a - b =",a - b);
+// console.log("a * b =",a * b);
+// console.log("a / b =",a / b);
+// console.log("a % b =",a % b);
+// console.log("a ** b =",a **b);
+
+// a + b = 20
+// script.js:7 a - b = 10
+// script.js:8 a * b = 75
+// script.js:9 a / b = 3
+// script.js:10 a % b = 0
+// script.js:11 a ** b = 759375
+
+
+// Assignment operators: =, +=, -=,....,**=
+
+// let a=10;
+// console.log("a =",a);
+
+// a += 5
+// console.log("a =",a);
+
+// a -=2
+// console.log("a =",a);
+
+// a *=4
+// console.log("a =",a);
+
+// a /=2
+// console.log("a =",a);
+
+// a %=3
+// console.log("a =",a);
+
+// a **=3
+// console.log("a =",a);
+
+// o/p
+// a = 10
+// script.js:19 a = 15
+// script.js:22 a = 13
+// script.js:25 a = 52
+// script.js:28 a = 26
+// script.js:31 a = 2
+// script.js:34 a = 8
+
+
+// increment and Decremnet operator
+
+// let a =  5;
+// console.log("a =",a)
+// console.log("a =",++a)
+// console.log("a =",a++)
+// console.log("a =",a)
+
+//  o/p a = 5
+// script.js:51 a = 6
+// script.js:52 a = 6
+// script.js:53 a = 7
+
+// let b = 5;
+// console.log("b =",b)
+// console.log("b =",--b)
+// console.log("b =",b--)
+// console.log("b =",b)
+
+
+// o/p b = 5
+// script.js:62 b = 4
+// script.js:63 b = 4
+// script.js:64 b = 3
+
+// Comparisum Operator: < , >, <=, >=, ==, != ,===, !==,  Ternary operator=?, :
+// Exreption create sathi
+
+// let a = 
+// Uncaught SyntaxError: Unexpected end 
+// of input (at script.js:74:9)
+
+// let a = 20;
+// let b = 10;
+// console.log("a < b=", a < b)
+// console.log("a > b=", a >b)
+// console.log("a <= b=", a <= b)
+// console.log("a >= b=", a >= b)
+// console.log("a == b=", a == b)
+// console.log("a != b=", a != b)
+
+// a < b= false
+// script.js:82 a > b= true
+// script.js:83 a <= b= false
+// script.js:84 a => b= true
+// script.js:85 a == b= false
+// script.js:86 a != b= true
+
+// let c ="20";
+// console.log("a == c=", a == c)
+// console.log("a === c=", a === c)
+// console.log("a != c=", a != c)
+// console.log("a !== c=", a !== c)
+
+// a == c= true
+// script.js:97 a === c= false
+// script.js:98 a != c= false
+// script.js:99 a !== c= true
+
+// Logical Operator: &&, ||, !
+
+// T && F : T
+// f|| f :f
+
+// console.log(20<40 && 9=="9")
+// console.log(20<40 && 9==="9")
+// console.log(20<40 || 9=="9")
+// console.log(20>40 || 9=="10")
+// console.log(!(70 < 2))
+// console.log(!(2 <= 2))
+
+// true
+// script.js:119 false
+// script.js:120 true
+// script.js:121 false
+// script.js:122 true
+// script.js:123 false
+
+
+
