ebe51bf7548e8f92643023d7dce26e20ecf2a22d
Komal Bagal <bagalkomal75@gmail.com>
2024-09-11 19:24:48 +0530

    Add files via upload

---
+++ 9 Aug practice.js
@@ -0,0 +1,126 @@
+
+// 1.Write a program for Even and Odd.
+
+let a = 20;
+if (a % 2 == 0) {
+  console.log(" Number is Even");
+} else {
+  console.log("Number is Odd");
+}
+
+//*****O/P: Number is Even
+
+//2.Write a program for number is Divisible by 17 or not.
+
+let c = 17;
+if(c%17 == 0){
+    console.log("The given number is Divisible by 17");
+}else{
+    console.log("The Given number is not divisible by 17");
+}
+
+//*****O/P: The given number is Divisible by 17
+
+//3. Write a program for display minimum of two numbers.
+
+let p =28;
+let q =45;
+if(p>q){
+    console.log("p is Minimum number");
+}
+else if(p<q)
+{
+    console.log("q is Minimum Number");
+}
+else
+{
+    console.log("Both Number is Equal");
+}
+
+//*****O/P: q is Minimum Number
+
+//4. Write a program to check number is positive or not.
+
+let n = 40;
+if(n>0){
+    console.log("Number is Positive");
+}
+else if(n<0){
+    console.log("Number is Negative");
+}
+
+//*****O/P: Number is Positive
+
+//5.Wrire a program to Accept x,y,z number from user and check a number is betwwen y & z.
+
+let x = 23;
+let y = 45;
+let z = 24;
+if(x>y && a<z){
+    console.log("x is between y and z");
+}
+else{
+    console.log("x is Not between y and z");
+}
+
+//*****O/P: x is Not between y and z
+
+// 6.Area of Circle.
+
+let A;
+let r = 20;
+A=3.14*r*r;{
+    console.log("Area=",A)
+}
+
+//*****O/P: Area= 1256
+
+
+//7. Area of rectangle.
+
+let L=100;
+let b=12;
+let A;
+A = L*b;
+{
+    console.log("Area of Rectangle=",A)
+}
+
+//*****O/P:Area of Rectangle= 1200
+
+//8.Addition of Two numbers.
+
+let s = 50;
+let h = 50;
+let k;
+k=s+h;
+{
+    console.log("Addition=",k)
+}
+
+//*****O/P: Addition= 100
+
+//9.Kinetic Energy.
+
+let M = 10;
+let v= 10;
+let Ke;
+Ke=0.5*M*v*v;
+{
+    console.log("kinetic energy is=",Ke);
+}
+
+//*****O/P: kinetic energy is= 500
+
+//10.Potential Energy.
+
+let m = 10;
+let h = 20;
+let g = 9.8;
+let pe;
+pe=m*g*h;
+{
+    console.log("potential is=",pe);
+}
+
+// *****O/P: potential is= 1960.
