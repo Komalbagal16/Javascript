a444e7047522dc5f2d26c56551dba2dc9d14ea7b
Komal Bagal <bagalkomal75@gmail.com>
2024-09-11 19:40:42 +0530

    Add files via upload

---
+++ 18 Aug index.js
@@ -0,0 +1,83 @@
+// math module
+
+// The math module is a build in module in javascripttheat provides
+//mathametical functions for trignomatry, exponential, lodarithmic
+//and other mathmatical operations.
+
+// console.log(Math.PI);
+// o/p:3.141592653589793
+
+// console.log(Math.E)
+//o/p:2.718281828459045
+
+a = 2.5;
+console.log(a);
+//2.5
+
+console.log(Math.floor(a));
+//2
+
+console.log(Math.ceil(a));
+//3
+
+console.log(Math.trunc(a));
+//2
+
+console.log(Math.round(a));
+//3
+
+// console.log(Math.max(23, 14, 94, 74, 84));
+//94
+
+console.log(Math.min(a));
+//2.5
+
+console.log(Math.pow(5, 4));
+//625
+
+console.log(Math.random());
+//0.14507660250012
+
+console.log(Math.random() * 10);
+//6.543523567453602
+
+console.log(Math.floor(Math.random() * 10));
+//6
+
+console.log(Math.floor(Math.random() * 10) + 1);
+//5
+
+console.log(Math.floor(Math.random() * (30 - 20) + 20));
+//20
+
+let max = 23;
+let min = 21;
+console.log(Math.floor(Math.random() * (max - min)) + min);
+//22
+
+console.log(Math.floor(Math.random() * (max - min + 1)) + min);
+//21
+
+console.log(Math.sqrt(225));
+//15
+
+console.log(Math.abs(-34));
+//34
+
+console.log(Math.hypot(60, 11)); //60.11------>;
+//61
+
+console.log(Math.log(10));
+//2.302585092994046
+
+console.log(Math.exp(5));
+//148.4131591025766
+
+console.log(Math.sin(Math.PI / 2));
+//1
+
+console.log(Math.cos(0));
+//1
+
+console.log(Math.tan(0));
+//0
