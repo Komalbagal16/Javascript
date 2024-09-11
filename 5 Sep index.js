a2c987494cd92e37592a082699dfa4aeb4e31838
Komal Bagal <bagalkomal75@gmail.com>
2024-09-11 19:56:42 +0530

    Add files via upload

---
+++ 5 Sep index.js
@@ -0,0 +1,52 @@
+//Window Object
+// let b = document.getElementById("btn");
+// b.addEventListener("click",
+//    function() {
+//     window.alert("button Clicked");
+//    } )
+
+//... sprade operator
+
+// var b = document.getElementById("btn");
+// b.addEventListener("click",
+//    function() {
+//     // window.open("https://www.apple.com/in/","_blank");
+//     // window.open("https://www.apple.com/in/","_self");
+//     window.open("https://www.apple.com/in/","_blank",
+//         "width=200,height=150,top=200,left=400");
+//     }
+// )
+//_blank= New tab
+
+// var b = document.getElementById("btn");
+// b.addEventListener("click",
+//    function() {
+//     console.log(window.innerHeight);
+//     console.log(window.outerHeight);
+//     console.log(window.innerWidth);
+//     console.log(window.outerWidth);
+//    })
+
+//location Object:
+
+//    b.addEventListener("click", function(){
+// location.href="https://www.example.com"//proprerties
+// location.assign("https:/www.example.com")//internal Calling
+// location.replace("https://www.example.com")//page change but magil record remove
+//  location.reload()//all record reload
+//    })
+
+//navigator object
+
+// var b = document.getElementById("btn");
+// b.addEventListener("cick", function () {
+//   console.log(navigator.userAgent);
+//   console.log(navigator.cookieEnabled);// if else
+// });
+
+//Screen object:
+
+// var b = document.getElementById("btn");
+// b.addEventListener("click", function (){
+//   console.log(screen);
+// });
