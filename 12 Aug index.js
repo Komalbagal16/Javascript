192f8c8ebf960a46e2acf29487baa950f13795af
Komal Bagal <bagalkomal75@gmail.com>
2024-09-11 19:26:44 +0530

    Add files via upload

---
+++ 12 Aug index.js
@@ -0,0 +1,120 @@
+
+// alert("Welocome!")
+
+// let res=confirm("Do you want to Quit?")
+// console.log(res)
+
+//  let age=prompt("enter your age:");
+//  console.log("Age =",age);
+
+//  Age = 21
+
+// let age=prompt("enter your age:",20);
+// console.log("Age =",age)
+// console.log(typeof age)
+
+// Age = 
+// index.js:14 string
+
+// let age=prompt("enter your age:");
+// console.log((age>=18)?"greater" : "less");
+
+// greater
+
+// let res=prompt("Do you Pass HSC Exam? (y/n):");
+// let ans = res =='y'? 
+// prompt("Which group do you have?") : "Fail"
+// console.log(ans);
+
+// let res=prompt("Do you Pass HSC Exam? (y/n):");
+// switch(res){
+//     case "y":
+//     case "yes":
+//     case "1":
+//     case "Y":
+//         console.log("You are eliible for higher studies");
+//         break;
+//     case "n":
+//     case "No":
+//     case "0":
+//         console.log("You are enot eliible for higher studies");
+//         break;
+//         default:
+//             console.log("invalid input");
+// }
+
+// You are enot eliible for higher studies
+
+// let a= 10;
+// let b= 25;
+// let operator = prompt("enter operation: ");
+// switch(operator){
+//     case "+":
+//     console.log(a+b);
+//     break;
+//     case "-":
+//     console.log(a-b);
+//     break;
+//     case "*":
+//     console.log(a*b);
+//     break;
+//     case "/":
+//     console.log(a/b);
+//     break;
+//     default:
+//         console.log("Invalid Operators");
+// }
+
+// -15
+//  35
+// 250
+// 0.4
+
+// Betwise operator : &, |, or, <<,>>,^
+
+// console.log("a & b:",13 & 26);
+// console.log("a | b:",13 | 26);
+// console.log("a ^ b:",13 ^ 16);
+
+// a & b: 8
+// index.js:76 a | b: 31
+// index.js:77 a ^ b: 29
+
+// let a= 2;
+// console.log( a << 4);
+// console.log(40 >> 1);
+
+// 32
+// 20
+
+// let b= 5;
+// console.log(~b)
+
+// -6
+
+
+// let n = (prompt("Enter the number:"));
+// let sum = 0;
+// let p = 1;
+
+// if(n > 0) {
+//   let n1 = n % 10;
+//   n = Math.floor(n / 10);
+//   sum += n1 * p;
+//   p *= 2;
+// }
+
+// // console.log("Decimal =", sum);
+
+// let n = (prompt("Enter the number:"));
+// let sum = 0;
+// let p = 1;
+
+// if(n > 0) {
+//   let n1 = n % 2;
+//   n = Math.floor(n / 2);
+//   sum += n1 * p;
+//   p *= 10;
+// }
+
+// console.log("Decimal =", sum);
